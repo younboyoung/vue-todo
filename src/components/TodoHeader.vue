5e25f114c5f49822ff1399c787e4af17db0f3d67
byyoun <byyoun@osci.kr>
2022-05-01 20:55:15 +0900

    feat: task completed

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1>TODO it!</h1>
+  </div>
+</template>
+
+// scoped : css 코드가 이 파일에서만 유효하게 함
+<style scoped>  
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import Vue from 'vue'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+// add necessary dependencies...
+
+app.mount("#app");
+
+/* eslint-disable no-new */
+// new Vue({
+//     el: '#app',
+//     components: { App },
+//     template: '<App/>'
+// })
